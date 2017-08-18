3f9f6b0e531c4ea4ba999967e002c7039c8e0f4d
eXt-Ra <vanardois.romain@gmail.com>
2017-08-18 09:34:41 +0200

    Middleware Access

--- CloudKit/App_Start/WebApiConfig.cs
+++ CloudKit/App_Start/WebApiConfig.cs
@@ -1,18 +1,30 @@
 ﻿using System.Net.Http.Headers;
 using System.Web.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace CloudKit
 {
     public static class WebApiConfig
     {
+		public static MongoDB.Driver.MongoClient client;
+		public static IMongoDatabase database;
+		public static IMongoCollection<BsonDocument> collection;
+		
         public static void Register(HttpConfiguration config)
         {
             config.EnableCors();
 
+			client = new MongoClient("mongodb://RomainHori:Dealtis25-@localhost:27017/Horizon");
+			database = client.GetDatabase("Horizon");
+			collection = database.GetCollection<BsonDocument>("userapis");
+			
             // Web API configuration and services
-            config.Formatters.JsonFormatter.SupportedMediaTypes
+			config.Formatters.JsonFormatter.SupportedMediaTypes
 	            .Add(new MediaTypeHeaderValue("text/html"));
-            
+
+            config.Filters.Add(new TokenMiddleware());
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ CloudKit/Middleware/TokenMiddleware.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CloudKit
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class TokenMiddleware : AuthorizationFilterAttribute
+    {
+        private const string _authorizedToken = "x-access-token";
+        private const string _userAgent = "User-Agent";
+
+        private UserAuthorizations objAuth = null;
+
+        public override void OnAuthorization(HttpActionContext filterContext)
+        {
+            string authorizedToken = string.Empty;
+            string userAgent = string.Empty;
+
+            try
+            {
+                if (filterContext.Request.Headers.Referrer != null)
+                {
+                    if (filterContext.Request.Headers.Referrer.AbsoluteUri != "http://localhost:8888/")
+                    {
+                        var headerToken = filterContext.Request.Headers.SingleOrDefault(x => x.Key == _authorizedToken);
+                        if (headerToken.Key != null)
+                        {
+                            authorizedToken = headerToken.Value.SingleOrDefault();
+                            userAgent = Convert.ToString(filterContext.Request.Headers.UserAgent);
+                            if (!IsAuthorize(authorizedToken, userAgent))
+                            {
+                                filterContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            filterContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    var headerToken = filterContext.Request.Headers.SingleOrDefault(x => x.Key == _authorizedToken);
+                    if (headerToken.Key != null)
+                    {
+                        authorizedToken = headerToken.Value.SingleOrDefault();
+                        userAgent = Convert.ToString(filterContext.Request.Headers.UserAgent);
+                        if (!IsAuthorize(authorizedToken, userAgent))
+                        {
+                            filterContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        filterContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                filterContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                return;
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+
+        private bool IsAuthorize(string authorizedToken, string userAgent)
+        {
+            objAuth = new UserAuthorizations();
+            bool result = false;
+            try
+            {
+                result = objAuth.ValidateToken(authorizedToken, userAgent);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
